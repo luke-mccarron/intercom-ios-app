7f7e2a86515eaff26c71fbcae1d6648c5362f1f3
Luke McCarron <luke.mccarron@intercom.io>
2025-07-11 10:21:14 -0500

    Intercom iOS app env setup

---
+++ Intercom/Intercom/IntercomApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntercomApp.swift
+//  Intercom
+//
+//  Created by Michael McCarron on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct IntercomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Intercom/IntercomTests/IntercomTests.swift
@@ -0,0 +1,17 @@
+//
+//  IntercomTests.swift
+//  IntercomTests
+//
+//  Created by Michael McCarron on 7/11/25.
+//
+
+import Testing
+@testable import Intercom
+
+struct IntercomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
